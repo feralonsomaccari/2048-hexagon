e8e8713687274d6e930009162f29ba2d0db39481
feralonsomaccari <feralonsomaccari@gmail.com>
2023-03-18 18:27:29 +0200

    Creates <Block/> component for hexagons

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,10 @@
 import React from "react"
+import Block from '../Block'
 
 export const App: React.FC = () => {
-  return <div>your game should be here</div>
+  return (
+  <div>
+    <Block/>
+  </div>
+  )
 }
---
+++ src/components/Block/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Block.module.css'
+
+const Block = () : JSX.Element => {
+  return (
+    <>
+        Block
+    </>
+  )
+}
+
+export default Block
