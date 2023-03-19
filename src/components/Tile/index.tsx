dd0cdf3f732ee872d248bb3c502fada80ec938e4
feralonsomaccari <feralonsomaccari@gmail.com>
2023-03-19 12:26:36 +0200

    Add movement event listeners and basic animations

--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -1,14 +1,43 @@
-import styles from './Tile.module.css'
+import { useEffect } from "react";
+import styles from "./Tile.module.css";
 
 type props = {
-    value: number;
-    style: {};
-  };
-  
-const Tile = ({value, style} : props) : JSX.Element => {
-  return (
-    <div className={styles.tile} style={style}>{value}</div>
-  )
-}
+  value: number;
+  style: {};
+};
 
-export default Tile
+const Tile = ({ value, style }: props): JSX.Element => {
+  const keyPressHandler = (event: KeyboardEvent): void => {
+    if (event.repeat) return;
+    switch (event.keyCode) {
+      case 81:
+        break;
+      case 87:
+        break;
+      case 69:
+        break;
+      case 65:
+        break;
+      case 83:
+        break;
+      case 68:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPressHandler);
+
+    return () => {
+      document.removeEventListener("keydown", keyPressHandler);
+    };
+  });
+
+  return (
+    <div className={styles.tile} style={style}>
+      {value}
+    </div>
+  );
+};
+
+export default Tile;
