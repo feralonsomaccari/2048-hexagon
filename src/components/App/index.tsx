53de7b6ee1260bae6031cae621e301046ba45d21
feralonsomaccari <feralonsomaccari@gmail.com>
2023-03-19 18:12:54 +0200

    Add basic server call

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -14,12 +14,6 @@ const hardcodedGrid = [
   { x: 1, y: -1, z: 0, value: 0 },
 ];
 
-const hardcodedServerResponse = [
-  { x: 0, y: 1, z: -1, value: 2 },
-  { x: 1, y: 0, z: -1, value: 2 },
-  { x: 1, y: -1, z: 0, value: 2 },
-];
-
 type coordinates = {
   x: number;
   y: number;
@@ -28,126 +22,171 @@ type coordinates = {
 };
 
 export const App: React.FC = () => {
-  const [grid, setGrid] = useState<coordinates[]>([])
-  const [tilesPos, setTilesPos] = useState<coordinates[]>([])
-  const [moving, setMoving] = useState(false)
-  
+  const [grid, setGrid] = useState<coordinates[]>([]);
+  const [tilesPos, setTilesPos] = useState<coordinates[]>([]);
+  const [moving, setMoving] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
+  const [firstCall, setFirstCall] = useState(true);
+
   useEffect(() => {
     document.addEventListener("keydown", keyPressHandler);
-    
+
     return () => {
       document.removeEventListener("keydown", keyPressHandler);
     };
   }, [tilesPos, moving]);
-  
+
   useEffect(() => {
-    setTilesPos(hardcodedServerResponse)
-
     const tempGrid = [...hardcodedGrid];
-    tempGrid.forEach(serverCoords => serverCoords.value = 0)
+    tempGrid.forEach((serverCoords) => (serverCoords.value = 0));
 
-    hardcodedServerResponse.forEach(serverCoords => {
-      tempGrid.forEach(gridCoords => {
-        if(gridCoords.x === serverCoords.x 
-          && gridCoords.y === serverCoords.y 
-          && gridCoords.z === serverCoords.z ){
-            gridCoords.value = serverCoords.value
-          }
-      })
-    })
-    setGrid(tempGrid)
-  }, [])
+    setTilesPos([
+      {
+        x: 0,
+        y: -1,
+        z: 1,
+        value: 4,
+      },
+      {
+        x: 0,
+        y: 1,
+        z: -1,
+        value: 4,
+      },
+      {
+        x: 1,
+        y: -1,
+        z: 0,
+        value: 2,
+      },
+      {
+        x: -1,
+        y: 0,
+        z: 1,
+        value: 4,
+      },
+      {
+        x: -1,
+        y: 0,
+        z: 1,
+        value: 2,
+      },
+    ]);
+    tilesPos.forEach((serverCoords) => {
+      tempGrid.forEach((gridCoords) => {
+        if (gridCoords.x === serverCoords.x && gridCoords.y === serverCoords.y && gridCoords.z === serverCoords.z) {
+          gridCoords.value = serverCoords.value;
+        }
+      });
+    });
+    setGrid(tempGrid);
+    //serverCall();
+  }, []);
 
   useEffect(() => {
     const tempGrid = [...grid];
-    
-    tempGrid.forEach(serverCoords => serverCoords.value = 0)
 
-    tilesPos.forEach(serverCoords => {
-      tempGrid.forEach(gridCoords => {
-        if(gridCoords.x === serverCoords.x 
-          && gridCoords.y === serverCoords.y 
-          && gridCoords.z === serverCoords.z ){
-            gridCoords.value = serverCoords.value
-          }
-      })
-    })
+    tempGrid.forEach((serverCoords) => (serverCoords.value = 0));
+
+    tilesPos.forEach((serverCoords) => {
+      tempGrid.forEach((gridCoords) => {
+        if (gridCoords.x === serverCoords.x && gridCoords.y === serverCoords.y && gridCoords.z === serverCoords.z) {
+          gridCoords.value = serverCoords.value;
+        }
+      });
+    });
 
     setTimeout(() => {
-      setMoving(false)
-    },100)
+      setMoving(false);
+    }, 100);
+  }, [tilesPos]);
 
-  }, [tilesPos])
+  const serverCall = async () => {
+    const serverResponse = await fetch("http://localhost:13337/2", {
+      method: "POST",
+      body: JSON.stringify(tilesPos),
+    });
+    const data: coordinates[] = await serverResponse.json();
+    if (data.length && !firstCall) {
+      setGameOver(true);
+      return;
+    }
+    console.log("pepe");
+    setFirstCall(true);
+    console.log([...data, ...tilesPos]);
+    setTilesPos([...data, ...tilesPos]);
+  };
 
-  const updateTilesPos = (direction : string) => {
-    setMoving(true)
+  const updateTilesPos = (direction: string) => {
+    setMoving(true);
 
     const removeTiles: number[] = [];
     const newTilesPos = [...tilesPos].map((tile) => {
-      const tempPos = {...tile}
-      for(let i = 0; i <= grid.length; i++) {
-        cubeMovement(tempPos, direction)
-        const checkGridBlock = grid.filter(block => tempPos.x === block.x && tempPos.y === block.y && tempPos.z === block.z)
-        if(checkGridBlock.length){
-          if(!checkGridBlock[0].value){
-            tile.x = checkGridBlock[0].x
-            tile.y = checkGridBlock[0].y
-            tile.z = checkGridBlock[0].z
+      const tempPos = { ...tile };
+      for (let i = 0; i <= grid.length; i++) {
+        cubeMovement(tempPos, direction);
+        const checkGridBlock = grid.filter((block) => tempPos.x === block.x && tempPos.y === block.y && tempPos.z === block.z);
+        if (checkGridBlock.length) {
+          if (!checkGridBlock[0].value) {
+            tile.x = checkGridBlock[0].x;
+            tile.y = checkGridBlock[0].y;
+            tile.z = checkGridBlock[0].z;
             continue;
-          }else{
-            if(tempPos.value === checkGridBlock[0].value) {
+          } else {
+            if (tempPos.value === checkGridBlock[0].value) {
               [...tilesPos].forEach((tile, index) => {
-                if(tile.x === checkGridBlock[0].x && tile.y === checkGridBlock[0].y && tile.z === checkGridBlock[0].z){
-                  removeTiles.push(index)
+                if (tile.x === checkGridBlock[0].x && tile.y === checkGridBlock[0].y && tile.z === checkGridBlock[0].z) {
+                  removeTiles.push(index);
                 }
-              })
-              tile.x = checkGridBlock[0].x
-              tile.y = checkGridBlock[0].y
-              tile.z = checkGridBlock[0].z
-              tile.value = tile.value + checkGridBlock[0].value
+              });
+              tile.x = checkGridBlock[0].x;
+              tile.y = checkGridBlock[0].y;
+              tile.z = checkGridBlock[0].z;
+              tile.value = tile.value + checkGridBlock[0].value;
             }
             break;
           }
-        }else{
+        } else {
           break;
         }
       }
-      return tile
-    })
+      return tile;
+    });
 
     removeTiles.forEach((tileIndex) => {
-      newTilesPos.splice(tileIndex,1);
-    })
+      newTilesPos.splice(tileIndex, 1);
+    });
 
-    setTilesPos(newTilesPos)
-  }
+    setTilesPos(newTilesPos);
+    //serverCall();
+  };
 
   const keyPressHandler = (event: KeyboardEvent): void => {
-    if (event.repeat || moving) return;
+    if (event.repeat || moving || gameOver) return;
     switch (event.key) {
       case "q":
       case "Q":
-        updateTilesPos('northWest')
+        updateTilesPos("northWest");
         break;
       case "w":
       case "W":
-        updateTilesPos('north')
+        updateTilesPos("north");
         break;
       case "e":
       case "E":
-        updateTilesPos('northEast')
+        updateTilesPos("northEast");
         break;
       case "a":
       case "A":
-        updateTilesPos('southWest')
+        updateTilesPos("southWest");
         break;
       case "s":
       case "S":
-        updateTilesPos('south')
+        updateTilesPos("south");
         break;
       case "d":
       case "D":
-        updateTilesPos('southEast')
+        updateTilesPos("southEast");
         break;
       case "f":
       case "F":
@@ -155,7 +194,7 @@ export const App: React.FC = () => {
     }
   };
 
-  if(!setTilesPos.length)  return <></>;
+  if (!setTilesPos.length) return <></>;
   return (
     <div className={styles.gameWrapper}>
       <div className={styles.gameContainer}>
