072f2f830e0e261b6252ec09c04d7e3b4c12f46c
feralonsomaccari <feralonsomaccari@gmail.com>
2023-03-19 15:03:16 +0200

    Add basic tile movement

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,49 +1,128 @@
-import React, { useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Block from "../Block";
 import Tile from "../Tile";
 import styles from "./App.module.css";
 import { getPositionFromCoords } from "./utils";
 
 const hardcodedGrid = [
-  { x: 0, y: 1, z: -1 },
-  { x: -1, y: 1, z: 0 },
-  { x: 1, y: 0, z: -1 },
-  { x: 0, y: 0, z: 0 },
-  { x: -1, y: 0, z: 1 },
-  { x: -0, y: -1, z: 1 },
-  { x: 1, y: -1, z: 0 },
+  { x: 0, y: 1, z: -1, value: 0 },
+  { x: -1, y: 1, z: 0, value: 0 },
+  { x: 1, y: 0, z: -1, value: 0 },
+  { x: 0, y: 0, z: 0, value: 0 },
+  { x: -1, y: 0, z: 1, value: 0 },
+  { x: -0, y: -1, z: 1, value: 0 },
+  { x: 1, y: -1, z: 0, value: 0 },
 ];
 
 const hardcodedServerResponse = [
-  { x: 0, y: 1, z: -1, value: 2 },
-  { x: 1, y: 0, z: -1, value: 2 },
-  { x: 1, y: -1, z: 0, value: 2 },
+  { x: 0, y: 1, z: -1, value: 2, key: 'asd' },
+  // { x: 1, y: 0, z: -1, value: 2 },
+  // { x: 1, y: -1, z: 0, value: 2 },
 ];
 
+type coordinates = {
+  x: number;
+  y: number;
+  z: number;
+  value: number;
+};
+
 export const App: React.FC = () => {
+  const [grid, setGrid] = useState<coordinates[]>([])
+  const [tilesPos, setTilesPos] = useState<coordinates[]>([])
+  
   useEffect(() => {
     document.addEventListener("keydown", keyPressHandler);
-
+    
     return () => {
       document.removeEventListener("keydown", keyPressHandler);
     };
-  });
+  }, [tilesPos]);
+  
+  useEffect(() => {
+    setTilesPos(hardcodedServerResponse)
+    const tempGrid = [...hardcodedGrid];
+    hardcodedServerResponse.forEach(serverCoords => {
+      tempGrid.forEach(gridCoords => {
+        if(gridCoords.x === serverCoords.x 
+          && gridCoords.y === serverCoords.y 
+          && gridCoords.z === serverCoords.z ){
+            gridCoords.value = serverCoords.value
+          }
+      })
+    })
+    setGrid(tempGrid)
+
+  }, [])
+
+  if(!setTilesPos.length)  return <></>;
+
+
+  const updateTilesPos = (direction : string) => {
+      
+    const cube = (tilePos : coordinates) => {
+      if(direction === 'northWest'){
+        tilePos.x = tilePos.x -= 1
+        tilePos.y = tilePos.y += 1 
+      }
+      if(direction === 'north'){
+        tilePos.y = tilePos.y += 1
+        tilePos.z = tilePos.z -= 1 
+      }
+      if(direction === 'northEast'){
+        tilePos.x = tilePos.x += 1
+        tilePos.z = tilePos.z -= 1 
+      }
+      if(direction === 'southWest'){
+        tilePos.x = tilePos.x -= 1
+        tilePos.z = tilePos.z += 1 
+      }
+      if(direction === 'south'){
+        tilePos.y = tilePos.y -= 1
+        tilePos.z = tilePos.z += 1 
+      }
+      if(direction === 'southEast'){
+        tilePos.x = tilePos.x += 1
+        tilePos.y = tilePos.y -= 1 
+      }
+    }
+    const newTilesPos = [...tilesPos]
+    const updatedNewTilesPos = newTilesPos.map(tilePos => {
+        cube(tilePos)
+        return tilePos;
+    })
+    setTilesPos(updatedNewTilesPos)
+  }
 
   const keyPressHandler = (event: KeyboardEvent): void => {
     if (event.repeat) return;
-    switch (event.keyCode) {
-      case 81:
-        console.log("pepe");
+    switch (event.key) {
+      case "q":
+      case "Q":
+        updateTilesPos('northWest')
         break;
-      case 87:
+      case "w":
+      case "W":
+        updateTilesPos('north')
         break;
-      case 69:
+      case "e":
+      case "E":
+        updateTilesPos('northEast')
         break;
-      case 65:
+      case "a":
+      case "A":
+        updateTilesPos('southWest')
         break;
-      case 83:
+      case "s":
+      case "S":
+        updateTilesPos('south')
         break;
-      case 68:
+      case "d":
+      case "D":
+        updateTilesPos('southEast')
+        break;
+      case "f":
+      case "F":
         break;
     }
   };
@@ -51,11 +130,11 @@ export const App: React.FC = () => {
   return (
     <div className={styles.gameWrapper}>
       <div className={styles.gameContainer}>
-        {hardcodedServerResponse.map((tile) => (
-          <Tile style={getPositionFromCoords(tile)} value={tile.value} />
+        {tilesPos.map((tile, index) => (
+          <Tile key={index} style={getPositionFromCoords(tile)} value={tile.value} />
         ))}
-        {hardcodedGrid.map((coords, index) => (
-          <Block key={index} style={getPositionFromCoords(coords)} x={coords.x} y={coords.y} z={coords.z} value={0} />
+        {grid.map((coords, index) => (
+          <Block key={index} style={getPositionFromCoords(coords)} x={coords.x} y={coords.y} z={coords.z} value={coords.value} />
         ))}
       </div>
     </div>
--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -9,7 +9,11 @@ type props = {
 };
 
 const Block = ({ x, y, z, value = 0, style = {} }: props): JSX.Element => {
-  return <div data-testid="hexagon-el" style={style} className={styles.hexagon} data-x={x} data-y={y} data-z={z} data-value={value} />;
+  return <div data-testid="hexagon-el" style={style} className={styles.hexagon} data-x={x} data-y={y} data-z={z} data-value={value}>
+    {x} {' '}
+    {y} {' '}
+    {z} {' '}
+  </div>;
 };
 
 export default Block;
