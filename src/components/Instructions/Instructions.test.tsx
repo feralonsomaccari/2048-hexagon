88e1f619ff906f90f4775c1e0e603080a9823be5
feralonsomaccari <feralonsomaccari@gmail.com>
2023-03-21 19:36:43 +0200

    Add unit tests to Instructions component

--- src/components/Instructions/Instructions.test.tsx
+++ src/components/Instructions/Instructions.test.tsx
@@ -3,7 +3,15 @@ import { render, screen } from "@testing-library/react";
 import Instructions from ".";
 
 describe("<Instructions/>", () => {
-  it("", () => {
-    
+  it("should have 'howtoplay' in the how to play section", () => {
+    render(<Instructions />);
+    const instructionsEl = screen.getByTestId("instructions");
+    expect(instructionsEl).toHaveAttribute("id", "howtoplay");
+  });
+
+  it("should get the User to the 'Game' section", () => {
+    render(<Instructions />);
+    const anchorEl = screen.getByRole("link");
+    expect(anchorEl).toHaveAttribute("href", "#game");
   });
 });
--- src/components/Instructions/index.tsx
+++ src/components/Instructions/index.tsx
@@ -2,11 +2,10 @@ import styles from "./Instructions.module.css";
 
 const Instructions = () => {
   return (
-    <div className={styles.instructionsWrapper}>
-      <p className={styles.text} id="howtoplay">
+    <div data-testid="instructions" className={styles.instructionsWrapper} id="howtoplay">
+      <p className={styles.text}>
         <strong> HOW TO PLAY:</strong> Use your
-        <strong> q, w, e, a, s, d</strong> keys to move the tiles. Tiles with
-        the same number
+        <strong> q, w, e, a, s, d</strong> keys to move the tiles. Tiles with the same number
         <strong> merge into one</strong> when they touch. Add them up to reach
         <strong> 2048!</strong>{" "}
       </p>
