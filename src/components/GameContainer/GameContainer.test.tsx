6732c546ae92ba8e503cf0439849d9ae976ff3b0
feralonsomaccari <feralonsomaccari@gmail.com>
2023-03-25 00:04:10 +0200

    Update GameContainer component unit test

--- src/components/GameContainer/GameContainer.test.tsx
+++ src/components/GameContainer/GameContainer.test.tsx
@@ -1,75 +1,10 @@
 import { render, screen } from "@testing-library/react";
-
+import {tileSet, grid } from "./dummyData"
 import GameContainer from ".";
 
 const props = {
-  tileSet: [
-    {
-      x: 1,
-      y: 0,
-      z: -1,
-      value: 2,
-      id: 0.519859284427105,
-    },
-    {
-      x: -1,
-      y: 1,
-      z: 0,
-      value: 2,
-      id: 0.9714689909376073,
-    },
-    {
-      x: 0,
-      y: 1,
-      z: -1,
-      value: 2,
-      id: 0.41289545040663955,
-    },
-  ],
-  grid: [
-    {
-      x: -1,
-      y: 0,
-      z: 1,
-      value: 0,
-    },
-    {
-      x: -1,
-      y: 1,
-      z: 0,
-      value: 0,
-    },
-    {
-      x: 0,
-      y: -1,
-      z: 1,
-      value: 0,
-    },
-    {
-      x: 0,
-      y: 0,
-      z: 0,
-      value: 0,
-    },
-    {
-      x: 0,
-      y: 1,
-      z: -1,
-      value: 0,
-    },
-    {
-      x: 1,
-      y: -1,
-      z: 0,
-      value: 0,
-    },
-    {
-      x: 1,
-      y: 0,
-      z: -1,
-      value: 0,
-    },
-  ],
+  tileSet: tileSet,
+  grid: grid,
   isGameOver: false,
 };
 
