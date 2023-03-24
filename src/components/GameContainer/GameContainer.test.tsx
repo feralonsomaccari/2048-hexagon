b88c5b0a02020c13217db59bfacf69b70d43b6a8
feralonsomaccari <feralonsomaccari@gmail.com>
2023-03-25 00:40:48 +0200

    Update gameContainer component unit tests

--- src/components/GameContainer/GameContainer.test.tsx
+++ src/components/GameContainer/GameContainer.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import {tileSet, grid } from "./dummyData"
+import { tileSet, grid } from "./dummyData";
+import { createHexGrid } from "../../utils";
 import GameContainer from ".";
 
 const props = {
@@ -16,9 +17,38 @@ describe("<GameContainer/>", () => {
   });
 
   it("should render a Grid of radius 2", () => {
-    render(<GameContainer {...props} />);
+    const grid = createHexGrid(1) 
+    render(<GameContainer {...props} grid={grid} />);
     const gameContainerEl = screen.getAllByTestId("block");
     expect(gameContainerEl).toHaveLength(7);
+  });
+
+  it("should render a Grid of radius 3", () => {
+    const grid = createHexGrid(2) 
+    render(<GameContainer {...props} grid={grid} />);
+    const gameContainerEl = screen.getAllByTestId("block");
+    expect(gameContainerEl).toHaveLength(19);
+  });
+
+  it("should render a Grid of radius 4", () => {
+    const grid = createHexGrid(3) 
+    render(<GameContainer {...props} grid={grid} />);
+    const gameContainerEl = screen.getAllByTestId("block");
+    expect(gameContainerEl).toHaveLength(37);
+  });
+
+  it("should render a Grid of radius 5", () => {
+    const grid = createHexGrid(4) 
+    render(<GameContainer {...props} grid={grid} />);
+    const gameContainerEl = screen.getAllByTestId("block");
+    expect(gameContainerEl).toHaveLength(61);
+  });
+
+  it("should render a Grid of radius 6", () => {
+    const grid = createHexGrid(5) 
+    render(<GameContainer {...props} grid={grid} />);
+    const gameContainerEl = screen.getAllByTestId("block");
+    expect(gameContainerEl).toHaveLength(91);
   });
 
   it("should render a Game Over overlay", () => {
